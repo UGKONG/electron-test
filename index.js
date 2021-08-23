405b1ad58c6f40abd159fb3ffdfe91eabe49cbcd
UGKONG <jsw9330@icloud.com>
2021-08-23 09:55:11 +0900

    'first'

---
+++ index.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+const onClosed = () => mainWindow = null;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 640,
+    height: 480
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', onClosed);
+})
